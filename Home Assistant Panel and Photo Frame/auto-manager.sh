7feb126bd28dfb4fbc58de16a0eb9a6ed4febc79
Lost_In_Traffic <141317168+c0def0rc0ffee@users.noreply.github.com>
2025-05-13 14:18:55 +0100

    Update auto-manager.sh
    Left my username for the pi I test on, reverted it to just 'pi'

--- Home Assistant Panel and Photo Frame/auto-manager.sh
+++ Home Assistant Panel and Photo Frame/auto-manager.sh
@@ -2,14 +2,14 @@
 sleep 5
 
 # Start feh-killer (logs optional)
-nohup bash /home/rob/touch-to-kill-feh.sh > /home/rob/logs/feh-killer.log 2>&1 &
+nohup bash /home/pi/touch-to-kill-feh.sh > /home/pi/logs/feh-killer.log 2>&1 &
 
 sleep 2
 
 # Start feh auto-restart (every 15 min)
-nohup bash /home/rob/auto-restart-feh.sh > /home/rob/logs/feh-resurrect.log 2>&1 &
+nohup bash /home/pi/auto-restart-feh.sh > /home/pi/logs/feh-resurrect.log 2>&1 &
 
 # Start Chromium in kiosk mode
-nohup bash /home/rob/start-kiosk.sh > /home/rob/logs/chromium.log 2>&1 &
+nohup bash /home/pi/start-kiosk.sh > /home/pi/logs/chromium.log 2>&1 &
 
 exit 0
