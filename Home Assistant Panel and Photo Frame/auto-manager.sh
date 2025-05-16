52905c478424a820793ea6eda32eb7a87d219873
Lost_In_Traffic <141317168+c0def0rc0ffee@users.noreply.github.com>
2025-05-16 11:55:37 +0100

    Update auto-manager.sh
    Swapped and allowed extra time from chromium to 'settle' fixed a weird issue with feh not working correctly

--- Home Assistant Panel and Photo Frame/auto-manager.sh
+++ Home Assistant Panel and Photo Frame/auto-manager.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 sleep 5
+# Start Chromium in kiosk mode
+nohup bash /home/pi/start-kiosk.sh > /home/pi/logs/chromium.log 2>&1 &
+
+sleep 15
 
 # Start feh-killer (logs optional)
 nohup bash /home/pi/touch-to-kill-feh.sh > /home/pi/logs/feh-killer.log 2>&1 &
@@ -9,7 +13,5 @@ sleep 2
 # Start feh auto-restart (every 15 min)
 nohup bash /home/pi/auto-restart-feh.sh > /home/pi/logs/feh-resurrect.log 2>&1 &
 
-# Start Chromium in kiosk mode
-nohup bash /home/pi/start-kiosk.sh > /home/pi/logs/chromium.log 2>&1 &
 
 exit 0
